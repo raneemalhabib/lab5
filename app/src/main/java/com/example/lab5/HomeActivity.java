d824489146933d7e8db6c89176d4498bcd8d84e9
raneemalhabib <raneemalh111@gmail.com>
2023-05-01 12:05:14 +0300

    after finish

---
+++ app/src/main/java/com/example/lab5/HomeActivity.java
@@ -0,0 +1,31 @@
+package com.example.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class HomeActivity extends AppCompatActivity {
+    EditText username, password;
+    Button signup, signin;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
